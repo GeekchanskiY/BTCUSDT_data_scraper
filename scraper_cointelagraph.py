bde9ccc643225b75b4297cfd8c327acd807752e7
GeekchanskiY <astrovskiy.04@gmail.com>
2022-07-06 14:56:35 +0300

    cointelegraph.com bitcoin news scraper ready

---
+++ scraper_cointelagraph.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+def get_cointelegraph_news(driver: webdriver) -> list[dict]:
+    """
+        Get bitcoin news from https://cointelegraph.com/
+    :param driver:
+    :return:
+    """
+    output_data: list = []
+    driver.get("https://cointelegraph.com/tags/bitcoin")
+    articles = driver.find_elements(By.CLASS_NAME, "post-card-inline")
+    for article in articles:
+        output_data.append(
+            {
+                "link": article.find_element(By.TAG_NAME, "a").get_attribute("href"),
+                "name": article.find_element(By.CLASS_NAME, "post-card-inline__title").text,
+                "date": article.find_element(By.CLASS_NAME, "post-card-inline__date").get_attribute("datetime")
+            }
+        )
+
+    return output_data
+
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    data = get_cointelegraph_news(driver)
+    print(data)
