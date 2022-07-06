55bc88520f100644672d52f0b4e974c46885e06e
GeekchanskiY <astrovskiy.04@gmail.com>
2022-07-06 20:05:29 +0300

    utoday scraper ready

---
+++ scraper_utoday.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+def get_utoday_news() -> list[dict]:
+    output_data: list[dict] = []
+    r = requests.get("https://u.today/bitcoin-news").text
+    soup = BeautifulSoup(r, 'html.parser')
+    articles = soup.find_all("div", attrs={"class": "category-item"})
+    for article in articles:
+        time = datetime.strptime(article.find("div", attrs={"class": "humble--time"}).text, "%m/%d/%Y - %H:%M")
+        output_data.append(
+            {
+                "name": article.find("div", attrs={"class": "category-item__title"}).text,
+                "link": article.find("a", attrs={"class": "category-item__title-link"})["href"],
+                "time": datetime.strftime(time, "%Y-%m-%d")
+            }
+        )
+
+    return output_data
+
+
+if __name__ == '__main__':
+    data = get_utoday_news()
+    print(data)
