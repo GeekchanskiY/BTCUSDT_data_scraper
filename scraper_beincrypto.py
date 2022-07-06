595987def99b0e95fd9b717d2ddd6a600e503126
GeekchanskiY <astrovskiy.04@gmail.com>
2022-07-06 18:10:51 +0300

    beincrypto scraper ready

---
+++ scraper_beincrypto.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+def get_beincrypto_news() -> list:
+    output_data: list[dict] =[]
+    r = requests.get("https://beincrypto.com/page/1/?s=bitcoin").text
+    soup = BeautifulSoup(r, 'html.parser')
+    cards = soup.find_all("article", attrs={"class": "multi-news-card"})
+    for card in cards:
+        date = datetime.strptime(card.find("span", attrs={"class": "date"}).text, "%b %d, %Y")
+        str_date = date.strftime("%Y-%m-%d")
+        output_data.append(
+            {
+                "link": card.find("a")["href"],
+                "name": card.find("div", attrs={"class": "title"}).text.replace("\n", ""),
+                "date": str_date
+            }
+        )
+    return output_data
+
+
+if __name__ == '__main__':
+    a = get_beincrypto_news()
+    print(a)
