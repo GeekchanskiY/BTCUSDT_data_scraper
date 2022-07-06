43bd3f4d033e048056ead5306c7c9b1c8bae0b8a
GeekchanskiY <astrovskiy.04@gmail.com>
2022-07-06 22:01:42 +0300

    scraper dailyhold ready

---
+++ scraper_dailyhodl.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from datetime import datetime
+
+
+def get_dailyhold_news(driver: webdriver) -> list[dict]:
+    output_data: list[dict] = []
+    driver.get("https://dailyhodl.com/bitcoin-news/")
+    articles = driver.find_elements(By.TAG_NAME, "article")
+    for article in articles:
+        time = article.find_element(By.CLASS_NAME, "jeg_meta_date").text
+        print(time)
+        date = datetime.strptime(time, "%B %d, %Y")
+        output_data.append({
+            "time": date.strftime("%Y-%m-%d"),
+            "name": article.find_element(By.TAG_NAME, "h3").text,
+            "link": article.find_element(By.TAG_NAME, "a").get_attribute("href")
+        })
+
+    return output_data
+
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    data = get_dailyhold_news(driver)
+    print(data)
