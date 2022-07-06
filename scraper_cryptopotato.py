a47f0c681cad0081ecfcd0737861030489e3f262
GeekchanskiY <astrovskiy.04@gmail.com>
2022-07-06 21:47:30 +0300

    scraper cryptopotato test (cloudflare pass)

--- scraper_cryptopotato.py
+++ scraper_cryptopotato.py
@@ -1,25 +1,23 @@
 import time
-
+import undetected_chromedriver
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 
-def get_cryptopotato_news(driver: webdriver) -> list[dict]:
+def get_cryptopotato_news() -> list[dict]:
     output_data: list[dict] = []
-    driver.get("https://cryptopotato.com/?s=bitcoin")
-    handle = driver.current_window_handle
-    driver.service.stop()
-    time.sleep(6)
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver = undetected_chromedriver.Chrome()
+
+
     driver.get("https://cryptopotato.com/?s=bitcoin")
     time.sleep(5)
     articles = driver.find_elements(By.CLASS_NAME, "article")
     print(len(articles))
     return output_data
 
+
 if __name__ == '__main__':
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    data = get_cryptopotato_news(driver)
+    data = get_cryptopotato_news()
     print(data)
