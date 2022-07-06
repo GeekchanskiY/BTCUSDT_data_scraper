56370f7c327d5650ba311c1d70be749828d7632b
GeekchanskiY <astrovskiy.04@gmail.com>
2022-07-06 21:27:13 +0300

    scraper decrypt ready

--- scraper_decrypt.py
+++ scraper_decrypt.py
@@ -2,10 +2,28 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import datetime
 
 
 def get_decrypt_news(driver: webdriver) -> list[dict]:
+    import time
+    output_data: list[dict] = []
     driver.get("https://decrypt.co/search/all/bitcoin")
+    time.sleep(1)
+    articles = driver.find_elements(By.CLASS_NAME, "block")
+    for article in articles:
+        try:
+            time = article.find_element(By.TAG_NAME, "time").get_attribute("datetime").split("T")[0]
+        except:
+            continue
+        output_data.append({
+            "name": article.find_element(By.TAG_NAME, "h2").text,
+            "link": article.get_attribute("href"),
+            "time": time
+        })
+
+    return output_data
+
 
 
 if __name__ == '__main__':
