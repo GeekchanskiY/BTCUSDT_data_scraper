6ae075d647582162e78634555a737c86442bd240
GeekchanskiY <astrovskiy.04@gmail.com>
2022-07-08 02:00:10 +0300

    scraper binance DB integration

--- scraper_binance.py
+++ scraper_binance.py
@@ -1,32 +1,26 @@
 import time
 
 import requests
-import pandas as pd
+from database import DB
 from datetime import datetime
 
 
-class BinanceDataHolder:
-    def __init__(self):
+class BinanceDataScraper:
+    def __init__(self, database: DB):
         self.data: list[dict] = []
+        self.database: DB = database
 
     def parse_data(self):
         r = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT").json()
-        self.data.append({
-            "currency": r["symbol"],
-            "price": r["price"],
-            "time": str(datetime.now())
-        })
-
-    def print_dataframe(self):
-        print(self.dataframe)
+        self.database.add_price(symbol=r["symbol"], date=str(datetime.now()), price=r["price"])
 
 
 if __name__ == '__main__':
-    dataholder = BinanceDataHolder()
+    db = DB()
+    dataholder = BinanceDataScraper(db)
     for i in range(30):
         dataholder.parse_data()
         time.sleep(1)
 
-    for el in dataholder.data:
-        print("time:", el["time"], "price:", el["price"])
+    db.print_prices()
 
