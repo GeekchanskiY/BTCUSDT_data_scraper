918fea1ce9c034f72cc01c6ce0bf51e2226df1f8
GeekchanskiY <astrovskiy.04@gmail.com>
2022-07-08 17:16:32 +0300

    database updated

--- database.py
+++ database.py
@@ -5,16 +5,13 @@ class DB:
     def __init__(self):
         self.con: sqlite3.Connection = sqlite3.connect('data.db')
         self.cur: sqlite3.Cursor = self.con.cursor()
-        try:
-            self.cur.execute('''CREATE TABLE price_data
-                           (datetime text, symbol text, price real)''')
-        except sqlite3.OperationalError:
-            pass
-        try:
-            self.cur.execute('''CREATE TABLE news_data
-                           (date text, symbol text, title text, link text, time: text)''')
-        except sqlite3.OperationalError:
-            pass
+
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS price_data
+                       (datetime text, symbol text, price real)''')
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS news_data
+                           (date text, symbol text, title text, link text, time text,
+                           CONSTRAINT link_unique UNIQUE (link))''')
+
         self.con.commit()
 
     def add_price(self, symbol: str, date: str, price: float):
@@ -22,14 +19,18 @@ class DB:
         self.con.commit()
 
     def add_news(self, symbol: str, date: str, time: str, link: str, text: str):
-        self.cur.execute(f"INSERT INTO news_data VALUES ('{date}', '{symbol}', '{text}', '{link}', '{time}')")
+        self.cur.execute(f"INSERT OR IGNORE INTO news_data VALUES ('{date}', '{symbol}', '{text}', '{link}', '{time}')")
         self.con.commit()
 
     def print_prices(self):
         for row in self.cur.execute('SELECT * FROM price_data'):
             print(row)
 
+    def print_news(self):
+        for row in self.cur.execute('SELECT link FROM news_data'):
+            print(row)
+
 
 if __name__ == '__main__':
     database: DB = DB()
-    database.print_prices()
+    database.print_news()
