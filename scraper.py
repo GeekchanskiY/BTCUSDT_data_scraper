d99285b27b3bca32761bad4ee387d68241e5181e
GeekchanskiY <astrovskiy.04@gmail.com>
2022-07-08 19:00:25 +0300

    scrapers added

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+import time
+
 from scraper_utoday import get_utoday_news
 from scraper_bitcoin import get_bitcoin_news
 from scraper_decrypt import get_decrypt_news
@@ -12,14 +14,25 @@ from selenium.webdriver.chrome.service import Service
 
 
 class ScrapersHandler:
-    def __init__(self):
+    def __init__(self, delay_minutes: int):
         self.driver: webdriver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.db: DB = DB()
+        self.binance_data_scraper = BinanceDataScraper(self.db)
+        self.mainloop(delay_minutes*60)
+
+    def mainloop(self, delay_seconds: int):
+        while True:
+            self.binance_data_scraper.parse_data()
+            self.scrap_all_selenium()
+            self.scrap_all_requests()
+            time.sleep(delay_seconds)
 
     def scrap_all_selenium(self):
         data = get_dailyhold_news(self.driver)
         self.add_news_to_db(data)
         data = get_bitcoin_news(self.driver)
+        self.add_news_to_db(data)
+        data = get_decrypt_news(self.driver)
         self.add_news_to_db(data)
 
     def scrap_all_requests(self):
